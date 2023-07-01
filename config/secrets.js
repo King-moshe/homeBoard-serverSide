cb700bf2783f2a0cc4c9cdd7c431cde10befe058
Moshe Amar <moso2411@gmail.com>
2023-07-01 23:48:51 +0300

    add the config, env

--- config/secrets.js
+++ config/secrets.js
@@ -1,8 +1,9 @@
 //כל המשתנים המאובטחים יהיו מאוגדים בקובץ אחד
 // כגון שם משתמש של מסד, סיסמא של מסד, וסיקרט טוקן
+require ('dotenv').config()
 exports.config = {
     db_pass: "",
     db_user: "",
-    token_secret: "M&Y",
-    db_url: "mongodb+srv://yeudaShtern:206261448@cluster0.sm6fiys.mongodb.net/design1"
+    token_secret: process.env.SECRET_TOKEN,
+    db_url: process.env.DB_URL
 }
